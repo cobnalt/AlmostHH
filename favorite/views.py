63d1144066c02fbdc093cea9802644f993c34f8b
jaggrenko <black_dog-82@mail.ru>
2022-04-30 02:39:42 +0300

    favs_app to class based (fin; don't like mixin)

--- favorite/views.py
+++ favorite/views.py
@@ -3,9 +3,11 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
 from portal.models import Resume, Vacancy
-from django.views.generic import CreateView, DetailView, TemplateView, ListView
+from django.views.generic import TemplateView
+
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # TODO на уровне моделей реализовать?
 class SessionHandlerMixin():
@@ -17,10 +19,11 @@ class SessionHandlerMixin():
         response, model, key_word = args
         result = response.get(self.SLANG['response_key'])
         if result:
-            result = filter(lambda x: x[self.SLANG['key_word']] == key_word, result)
+            result = filter(lambda x: x[self.SLANG['key_word']] == key_word,
+                            result)
 
         return [get_object_or_404(model, pk=item[self.SLANG['model_key']])
-                  for item in result] if result else None
+                for item in result] if result else None
 
 
 class FavoritesList(LoginRequiredMixin, SessionHandlerMixin, TemplateView):
@@ -50,68 +53,105 @@ class FavoritesList(LoginRequiredMixin, SessionHandlerMixin, TemplateView):
 #                   {'res': res, 'vac': vac, 'left_menu': 'favs'})
 
 
-@login_required()
-def add_to_favorites(request):
-    if request.method == 'POST':
-        if not request.session.get('favorites'):
-            request.session['favorites'] = list()
-        else:
-            request.session['favorites'] = list(request.session['favorites'])
+class AddToFavorites(LoginRequiredMixin, TemplateView):
+    def post(self, request, *args, **kwargs):
+        sequence = dict(type=self.request.POST.get('type'),
+                        id=self.request.POST.get('id'))
+        session = self.request.session.get('favorites')
 
-        item_exist = next((item for item in request.session['favorites'] if
-                           item['type'] == request.POST.get('type') and item[
-                               'id'] == request.POST.get('id')), False)
+        session = [] if not session else session
 
-        add_data = {
-            'type': request.POST.get('type'),
-            'id': request.POST.get('id'),
-        }
+        if sequence not in session:
+            session.append(sequence)
 
-        if not item_exist:
-            request.session['favorites'].append(add_data)
-            request.session.modified = True
+            self.request.session.update({'favorites': session})
+            self.request.session.modified = True
 
-    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-        data = {
-            'type': request.POST.get('type'),
-            'id': request.POST.get('id'),
-            'count': len(request.session['favorites']),
-        }
-        request.session.modified = True
-        return JsonResponse(data)
+            sequence.update({'count': len(session)})
 
-    return redirect(request.POST.get('url_from'))
+            return JsonResponse(sequence)
 
 
-@login_required()
-def remove_from_favorites(request):
-    if request.method == 'POST':
-        for item in request.session['favorites']:
-            if item['type'] == request.POST.get('type') and item[
-                'id'] == request.POST.get('id'):
-                item.clear()
+# @login_required()
+# def add_to_favorites(request):
+#     if request.method == 'POST':
+#         if not request.session.get('favorites'):
+#             request.session['favorites'] = list()
+#         else:
+#             request.session['favorites'] = list(request.session['favorites'])
+#
+#         item_exist = next((item for item in request.session['favorites'] if
+#                            item['type'] == request.POST.get('type') and item[
+#                                'id'] == request.POST.get('id')), False)
+#         print(request.session['favorites'])
+#         add_data = {
+#             'type': request.POST.get('type'),
+#             'id': request.POST.get('id'),
+#         }
+#
+#         if not item_exist:
+#             request.session['favorites'].append(add_data)
+#             request.session.modified = True
+#
+#     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+#         data = {
+#             'type': request.POST.get('type'),
+#             'id': request.POST.get('id'),
+#             'count': len(request.session['favorites']),
+#         }
+#         request.session.modified = True
+#         return JsonResponse(data)
+#
+#     return redirect(request.POST.get('url_from'))
 
-        while {} in request.session['favorites']:
-            request.session['favorites'].remove({})
+class RemoveFromFavorites(LoginRequiredMixin, TemplateView):
+    def post(self, request, *args, **kwargs):
+        sequence = dict(type=self.request.POST.get('type'),
+                        id=self.request.POST.get('id'))
+        session = self.request.session.get('favorites')
 
-        if not request.session['favorites']:
-            del request.session['favorites']
+        session = [item for item in session if item['id'] != sequence['id']]
 
-        request.session.modified = True
+        self.request.session.update({'favorites': session})
+        self.request.session.modified = True
 
-    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-        data = {
-            'type': request.POST.get('type'),
-            'id': request.POST.get('id'),
-            'count': 0 if not request.session.get('favorites')
-            else len(request.session['favorites']),
-        }
-        request.session.modified = True
-        return JsonResponse(data)
+        sequence.update({'count': len(session)})
 
-    return redirect(request.POST.get('url_from'))
+        return JsonResponse(sequence)
 
 
-@login_required()
-def favorites_api(request):
-    return JsonResponse(request.session.get('favorites'), safe=False)
+# @login_required()
+# def remove_from_favorites(request):
+#     if request.method == 'POST':
+#         for item in request.session['favorites']:
+#             if item['type'] == request.POST.get('type') and item[
+#                 'id'] == request.POST.get('id'):
+#                 item.clear()
+#
+#         while {} in request.session['favorites']:
+#             request.session['favorites'].remove({})
+#
+#         if not request.session['favorites']:
+#             del request.session['favorites']
+#
+#         request.session.modified = True
+#
+#     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+#         data = {
+#             'type': request.POST.get('type'),
+#             'id': request.POST.get('id'),
+#             'count': 0 if not request.session.get('favorites')
+#             else len(request.session['favorites']),
+#         }
+#         request.session.modified = True
+#         return JsonResponse(data)
+#
+#     return redirect(request.POST.get('url_from'))
+
+class FavoritesApi(LoginRequiredMixin, TemplateView):
+    def get(self, request, *args, **kwargs):
+        return JsonResponse(request.session.get('favorites'), safe=False)
+
+# @login_required()
+# def favorites_api(request):
+#     return JsonResponse(request.session.get('favorites'), safe=False)
