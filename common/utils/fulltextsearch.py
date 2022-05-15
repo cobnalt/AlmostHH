bdd71524ee1207ee2083eac04d8ce07b698e0271
jaggrenko <black_dog-82@mail.ru>
2022-05-15 07:11:46 +0300

    fulltexsearch + postgre + ready + trigrams

--- common/utils/fulltextsearch.py
+++ common/utils/fulltextsearch.py
@@ -1,5 +1,5 @@
 from django.contrib.postgres.search import SearchVector, SearchQuery, \
-    SearchRank, SearchHeadline
+    SearchRank, SearchHeadline, TrigramSimilarity, TrigramDistance
 from django.db.models import Q, F, Value
 from django.db.models.functions import Concat
 
@@ -14,9 +14,21 @@ class SearchResultsList(ListView):
         search_query = SearchQuery(query)
         search_headline = SearchHeadline(self.headline_expression,
                                          search_query)
-        return self.model.objects.annotate(
+
+        result = self.model.objects.annotate(
             search=search_vector,
             rank=SearchRank(search_vector, search_query)
         ).annotate(annotate_expression=self.annotate_expression,
                    headline=search_headline).filter(
             search=search_query).order_by("-rank")
+
+        if not result:
+            return self.model.objects.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).annotate(
+                similarity=TrigramSimilarity(self.vector[0], query)
+                           + TrigramSimilarity(self.vector[1], query),
+            ).filter(similarity__gt=0.3).order_by("-similarity")
+
+        return result
