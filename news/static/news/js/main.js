4f002f0643859daa811eb158001a3bc03c4cf6c4
Affsyan <Affsyan@gmail.com>
2022-05-17 20:58:40 +0300

    Изменение фронта

--- news/static/news/js/main.js
+++ news/static/news/js/main.js
@@ -15,6 +15,8 @@ function getCookie(name) {
 }
 var csrftoken = getCookie('csrftoken');
 
+console.log(csrftoken);
+
 function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
